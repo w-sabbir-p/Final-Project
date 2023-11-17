2b23cf4f64af6bf24aea1fd85d0077e940a0c3c9
Wahid Sabbir <137522117+w-sabbir-p@users.noreply.github.com>
2023-11-17 18:27:59 +0600

    Add files via upload

---
+++ home_page.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:weather/weather.dart';
+import 'package:portfolio/consts.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final WeatherFactory _wf = WeatherFactory(OPENWEATHER_API_KEY);
+
+  Weather? _weather;
+
+  @override
+  void initState() {
+    super.initState();
+    _wf.currentWeatherByCityName("Dhaka").then((w) {
+      setState(() {
+        _weather = w;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black, // Set the background color to dark
+      body: _buildUI(),
+    );
+  }
+
+  Widget _buildUI() {
+    if (_weather == null) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _locationHeader(),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.08,
+          ),
+          _dateTimeInfo(),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          _weatherIcon(),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.02,
+          ),
+          _currentTemp(),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.02,
+          ),
+          _extraInfo(),
+        ],
+      ),
+    );
+  }
+
+  Widget _locationHeader() {
+    return Text(
+      _weather?.areaName ?? "",
+      style: const TextStyle(
+        fontSize: 60,
+        fontWeight: FontWeight.w500,
+        color: Colors.white, // Set text color to white
+      ),
+    );
+  }
+
+  Widget _dateTimeInfo() {
+    DateTime now = _weather!.date!;
+    return Column(
+      children: [
+        Text(
+          DateFormat("h:mm a").format(now),
+          style: const TextStyle(
+            fontSize: 35,
+            color: Colors.teal, // Set text color to white
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              DateFormat("EEEE").format(now),
+              style: const TextStyle(
+                fontWeight: FontWeight.w700,
+                color: Colors.teal, // Set text color to white
+              ),
+            ),
+            Text(
+              "  ${DateFormat("d.m.y").format(now)}",
+              style: const TextStyle(
+                fontWeight: FontWeight.w400,
+                color: Colors.white, // Set text color to white
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _weatherIcon() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height * 0.20,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                  "http://openweathermap.org/img/wn/${_weather?.weatherIcon}@4x.png"),
+            ),
+          ),
+        ),
+        Text(
+          _weather?.weatherDescription ?? "",
+          style: const TextStyle(
+            color: Colors.tealAccent, // Set text color to white
+            fontSize: 20,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _currentTemp() {
+    return Text(
+      "${_weather?.temperature?.celsius?.toStringAsFixed(0)}° C",
+      style: const TextStyle(
+        color: Colors.white, // Set text color to white
+        fontSize: 90,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+  }
+
+  Widget _extraInfo() {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.15,
+      width: MediaQuery.of(context).size.width * 0.80,
+      decoration: BoxDecoration(
+        color: Colors.teal,
+        borderRadius: BorderRadius.circular(
+          20,
+        ),
+      ),
+      padding: const EdgeInsets.all(
+        8.0,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Max: ${_weather?.tempMax?.celsius?.toStringAsFixed(0)}° C",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              Text(
+                "Min: ${_weather?.tempMin?.celsius?.toStringAsFixed(0)}° C",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Wind: ${_weather?.windSpeed?.toStringAsFixed(0)}m/s",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              Text(
+                "Humidity: ${_weather?.humidity?.toStringAsFixed(0)}%",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ main.dart
@@ -0,0 +1,283 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/Fragment/Apps.dart';
+import 'package:portfolio/Fragment/Skills.dart';
+import 'package:portfolio/Fragment/Experiences.dart';
+import 'package:portfolio/Fragment/About.dart';
+import 'package:portfolio/Fragment/Certificates.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomeActivity(),
+      theme: ThemeData(
+        primaryColor: Colors.black87,
+        hintColor: Colors.black87,
+        textTheme: const TextTheme(
+          headline6: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+          subtitle1: TextStyle(
+            fontSize: 20,
+            color: Colors.black87,
+          ),
+          subtitle2: TextStyle(
+            fontSize: 18,
+            color: Colors.black54,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 5, // Changed the length to 5 for the five tabs
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "My Portfolio",
+            style: Theme.of(context).textTheme.headline6!,
+          ),
+          backgroundColor: Theme.of(context).primaryColor,
+          bottom: const TabBar(
+            isScrollable: true,
+            tabs: [
+              Tab(
+                icon: Icon(Icons.person, color: Colors.tealAccent),
+                text: 'About',
+              ),
+              Tab(
+                icon: Icon(Icons.star, color: Colors.tealAccent),
+                text: 'Skills',
+              ),
+              Tab(
+                icon: Icon(Icons.work, color: Colors.tealAccent),
+                text: 'Experiences',
+              ),
+              Tab(
+                icon: Icon(Icons.apps_outage_outlined, color: Colors.tealAccent),
+                text: 'Apps',
+              ),
+              Tab(
+                icon: Icon(Icons.card_membership, color: Colors.tealAccent),
+                text: 'Certificates',
+              ),
+
+            ],
+          ),
+        ),
+        drawer: Drawer(
+          child: Container(
+            color: Colors.black54,
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                DrawerHeader(
+                  decoration: const BoxDecoration(
+                    color: Colors.black54,
+                  ),
+                  child: UserAccountsDrawerHeader(
+                    accountName: const Text(
+                      "Wahid Sabbir",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    accountEmail: const Text(
+                      "wahid15-3975@diu.edu.bd",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                    currentAccountPicture: ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Image.network(
+                        "https://media.licdn.com/dms/image/D5603AQFgIjUpihMMvA/profile-displayphoto-shrink_400_400/0/1691085262924?e=1701302400&v=beta&t=59Or-RojKMYIQQZt5z7qlkPmzu-QZca8OHoH3w6sxpw",
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.black54,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.home,
+                    color: Colors.tealAccent,
+                  ),
+                  title: Text(
+                    'Home',
+                    style: TextStyle(
+                      color: Colors.white, fontSize: 18,
+                    ),
+                  ),
+                  onTap: () {
+                    // Add logic for Home onTap
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.settings,
+                    color: Colors.tealAccent,
+                  ),
+                  title: Text(
+                    'Settings',
+                    style: TextStyle(
+                      color: Colors.white, fontSize: 18,
+                    ),
+                  ),
+                  onTap: () {
+                    // Add logic for Settings onTap
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.tealAccent,
+                  ),
+                  title: Text(
+                    'Gmail',
+                    style: TextStyle(
+                      color: Colors.white, fontSize: 18,
+                    ),
+                  ),
+                  onTap: () {
+                    launch('https://mail.google.com/mail/u/0/#inbox');
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.link,
+                    color: Colors.tealAccent,
+                  ),
+                  title: Text(
+                    'LinkedIn',
+                    style: TextStyle(
+                      color: Colors.white, fontSize: 18,
+                    ),
+                  ),
+                  onTap: () {
+                    launch('https://www.linkedin.com/in/wahidsabbir/');
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.facebook,
+                    color: Colors.tealAccent,
+                  ),
+                  title: Text(
+                    'Facebook',
+                    style: TextStyle(
+                      color: Colors.white, fontSize: 18,
+                    ),
+                  ),
+                  onTap: () {
+                    launch('https://www.facebook.com/w.sabbir.p');
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.camera_enhance_outlined,
+                    color: Colors.tealAccent,
+                  ),
+                  title: Text(
+                    'Instagram',
+                    style: TextStyle(
+                      color: Colors.white, fontSize: 18,
+                    ),
+                  ),
+                  onTap: () {
+                    launch('https://www.instagram.com/w.sabbir.p/');
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.account_tree_sharp,
+                    color: Colors.tealAccent,
+                  ),
+                  title: Text(
+                    'Github',
+                    style: TextStyle(
+                      color: Colors.white, fontSize: 18,
+                    ),
+                  ),
+                  onTap: () {
+                    launch('https://github.com/w-sabbir-p');
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.note_alt_sharp,
+                    color: Colors.tealAccent,
+                  ),
+                  title: Text(
+                    'Get CV',
+                    style: TextStyle(
+                      color: Colors.white, fontSize: 18,
+                    ),
+                  ),
+                  onTap: () {
+                    launch('https://drive.google.com/file/d/17MmjgrpJe5J0ekwBRC1XscZ-lnEhb0LK/view?usp=drivesdk');
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.logout,
+                    color: Colors.tealAccent,
+                  ),
+                  title: Text(
+                    'Logout',
+                    style: TextStyle(
+                      color: Colors.white, fontSize: 18,
+                    ),
+                  ),
+                  onTap: () {
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            About(),
+            Skills(),
+            Experiences(),
+            Apps(),
+            Certificates(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw "Could not launch $url";
+    }
+  }
+}
+
+// ... (AppListItem and Apps widget code remains unchanged)
